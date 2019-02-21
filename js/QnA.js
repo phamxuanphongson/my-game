8865812951e58793297908c60300b11e4ada7731
phamxuanphongson <phamxuanphongson@gmail.com>
2019-02-21 08:52:33 +0700

    upload

---
+++ js/QnA.js
@@ -0,0 +1,104 @@
+const myQuestions  = [
+{
+
+},
+{
+	cauHoi: "Đường nào là đường dài nhất ?",
+	triGia : "100.000 VND",
+	traLoi: {
+		A: "Đường chân trời",
+		B: "Đường làng",
+		C: "Đường xóm",
+		D: "Đường thốn-nốt"
+	},
+	dapAn: "Đường chân trời"
+},
+{
+	cauHoi: "Nhắn câu gì khiến chị em gục đổ ?",
+	triGia : "200.000 VND",
+	traLoi: {
+		A: "Em ăn cơm chưa ?",
+		B: "Em mặc áo ấm vào nhé",
+		C: "Anh yêu em nhiều lắm ",
+		D: "Em thật tuyệt vời !"
+	},
+	dapAn: "Em ăn cơm chưa ?"
+},
+{
+	cauHoi: "Cái gì 2 lỗ, có gió thì sống, không gió thì chết?",
+	triGia : "300.000 VND",
+	traLoi: {
+		A: "Lỗ lời",
+		B: "Ổ cắm",
+		C: "Lỗ c",
+		D: "Lỗ mũi"
+	},
+	dapAn: "Lỗ mũi"
+},
+{
+	cauHoi: "Cái gì sáng tối đánh cha, đánh mẹ, đánh cả gia đình ?",
+	triGia : "500.000 VND",
+	traLoi: {
+		A: "Cái thằng mất dậy",
+		B: "Cái bàn chải",
+		C: "Cái gậy",
+		D: "Cái boong"
+	},
+	dapAn: "Cái bàn chải"
+},
+{
+	cauHoi: "Con gì đập thì sống, không đập thì chết ?",
+	triGia : "1.000.000 VND",
+	traLoi: {
+		A: "Con nghiện",
+		B: "Con loăng quăng",
+		C: "Con súc vật",
+		D: "Con tim"
+	},
+	dapAn: "Con tim"
+},
+{
+	cauHoi: "Cũng là 1 loại 'Khá' nhưng mọi người lại rất ghét ?",
+	triGia : "2.000.000 VND",
+	traLoi: {
+		A: "Học sinh khá",
+		B: "Khá giỏi",
+		C: "Khá tốt",
+		D: "Khá bẢnH"
+	},
+	dapAn: "Khá bẢnH"
+},
+{
+	cauHoi: "Giữa số 0 và số 19 có bao nhiêu số 1 ?",
+	triGia : "4.000.000 VND",
+	traLoi: {
+		A: "8 số",
+		B: "11 số",
+		C: "12 số",
+		D: "13 số"
+	},
+	dapAn: "11 số"
+},
+{
+	cauHoi: "Quyền nào phải đấu tranh bằng nắm đấm ?",
+	triGia : "8.000.000 VND",
+	traLoi: {
+		A: "Quyền chill",
+		B: "Quyền chơi gaem",
+		C: "Quyền thủ",
+		D: "Quyền anh"
+	},
+	dapAn: "Quyền anh"
+},
+{
+	cauHoi: "Ai là tác giả của tác phẩm Chữ Người Tử Tù ?",
+	triGia : "16.000.000 VND",
+	traLoi: {
+		A: "Nguyễn Tuân",
+		B: "Ngô Tất Tố",
+		C: "Xuân Diệu",
+		D: "Lão Hạc"
+	},
+	dapAn: "Nguyễn Tuân"
+},
+];
---
+++ js/QnA2.js
@@ -0,0 +1,110 @@
+const myQuestions  = [
+{
+
+},
+{
+	cauHoi: "Đường nào là đường dài nhất ?",
+	triGia : "100.000 VND",
+	// traLoi: {
+	// 	A: "Đường chân trời",
+	// 	B: "Đường làng",
+	// 	C: "Đường xóm",
+	// 	D: "Đường thốn-nốt"
+	// },
+	traLoi: [
+		"Đường chân trời",
+		"Đường làng",
+		 "Đường xóm",
+		"Đường thốn-nốt"
+	],
+	dapAn: "Đường chân trời"
+},
+{
+	cauHoi: "Nhắn câu gì khiến chị em gục đổ ?",
+	triGia : "200.000 VND",
+	traLoi: [
+		"Em ăn cơm chưa ?",
+		"Em mặc áo ấm vào nhé",
+		"Anh yêu em nhiều lắm ",
+		"Em thật tuyệt vời !"
+	],
+	dapAn: "Em ăn cơm chưa ?"
+},
+{
+	cauHoi: "Cái gì 2 lỗ, có gió thì sống, không gió thì chết?",
+	triGia : "300.000 VND",
+	traLoi: [
+		"Lỗ lời",
+		"Ổ cắm",
+		"Lỗ c",
+		"Lỗ mũi"
+	],
+	dapAn: "Lỗ mũi"
+},
+{
+	cauHoi: "Cái gì sáng tối đánh cha, đánh mẹ, đánh cả gia đình ?",
+	triGia : "500.000 VND",
+	traLoi: [
+		"Cái thằng mất dậy",
+		"Cái bàn chải",
+		"Cái gậy",
+		"Cái boong"
+	],
+	dapAn: "Cái bàn chải"
+},
+{
+	cauHoi: "Con gì đập thì sống, không đập thì chết ?",
+	triGia : "1.000.000 VND",
+	traLoi: [
+		"Con nghiện",
+		"Con loăng quăng",
+		"Con súc vật",
+		"Con tim"
+	],
+	dapAn: "Con tim"
+},
+{
+	cauHoi: "Cũng là 1 loại 'Khá' nhưng mọi người lại rất ghét ?",
+	triGia : "2.000.000 VND",
+	traLoi: [
+		"Học sinh khá",
+		"Khá giỏi",
+		"Khá tốt",
+		"Khá bẢnH"
+	],
+	dapAn: "Khá bẢnH"
+},
+{
+	cauHoi: "Giữa số 0 và số 19 có bao nhiêu số 1 ?",
+	triGia : "4.000.000 VND",
+	traLoi: [
+		"8 số",
+		"11 số",
+		"12 số",
+		"13 số"
+	],
+	dapAn: "11 số"
+},
+{
+	cauHoi: "Quyền nào phải đấu tranh bằng nắm đấm ?",
+	triGia : "8.000.000 VND",
+	traLoi: [
+		"Quyền chill",
+		"Quyền chơi gaem",
+		"Quyền thủ",
+		"Quyền anh"
+	],
+	dapAn: "Quyền anh"
+},
+{
+	cauHoi: "Ai là tác giả của tác phẩm Chữ Người Tử Tù ?",
+	triGia : "16.000.000 VND",
+	traLoi: [
+		"Nguyễn Tuân",
+		"Ngô Tất Tố",
+		"Xuân Diệu",
+		"Lão Hạc"
+	],
+	dapAn: "Nguyễn Tuân"
+},
+];
